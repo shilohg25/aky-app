c5765eef15901354eee5307c448d181d2c110b1c
shilohg25 <shiloh.gelito@gmail.com>
2026-03-27 22:08:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const SUPABASE_URL = "https://pnadtkjdybaalnaqotiu.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_lnGONrjUkuM00sgdNTS7aQ_lSxolPAd";
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 (function () {
   "use strict";
 

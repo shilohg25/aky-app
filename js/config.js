ecde54216b2856d4158bbb2e0d621ca7f537223b
shilohg25 <shiloh.gelito@gmail.com>
2026-04-11 07:39:01 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://hghssblwbezmyrrfthri.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_IL6vSMDa-G1kpWCMR0KKXA_9NtUYIMr"
+  SUPABASE_URL: "https://pnadtkjdybaalnaqotiu.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_lnGONrjUkuM00sgdNTS7aQ_lSxolPAd"
 };
 
 window.ACCOUNT_ADMIN_FUNCTION_URL = `${window.APP_CONFIG.SUPABASE_URL}/functions/v1/account-admin`;
